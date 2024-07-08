271b23754dea49a0c996398ac33619a5c490dcfe
Matheus Henrique Pedroso Viana <matheus_pviana@hotmail.com>
2024-07-08 18:54:46 -0300

    OK

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/run-script')
+@app.route('/app3.py')
 def run_script():
-    subprocess.run(['python', 'your_script.py'])  # Altere 'your_script.py' para o nome do seu script Python
+    subprocess.run(['python', 'app3.py'])  # Altere 'your_script.py' para o nome do seu script Python
     return 'Script Executado!'
 
 if __name__ == '__main__':
